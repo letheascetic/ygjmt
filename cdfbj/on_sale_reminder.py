3f01c4beed58f406c0953e3047b67cdcfadae090
letheascetic <yizhifight@163.com>
2020-09-20 14:52:49 +0800

    update

--- cdfbj/on_sale_reminder.py
+++ cdfbj/on_sale_reminder.py
@@ -215,7 +215,7 @@ class OnSaleReminder(object):
         if new_num <= 0:
             return
 
-        url = 'http://http.tiqu.alicdns.com/getip3?num={0}&type=2&pro=&city=0&yys=0&port=11&pack=110169&ts=1&ys=0&cs=1&lb=1&sb=0&pb=4&mr=2&regions=&gm=4'
+        url = 'http://http.tiqu.alicdns.com/getip3?num={0}&type=2&pro=&city=0&yys=0&port=11&pack=110169&ts=1&ys=0&cs=1&lb=1&sb=0&pb=4&mr=2&regions='
         url = url.format(new_num)
 
         try:
